ab3de65014b4c6130a6190dcad5691d2110eab37
gstrauss42 <gstrauss42@users.noreply.github.com>
2020-03-09 13:37:12 +0200

    main code created

--- main.c
+++ main.c
@@ -1,9 +1,10 @@
 int     bracket_check(char *math_str);
 int     symbol_check(char *math_str);
+int     math(char *math_str);
 #include <stdio.h>
 
 int main(int argc, char **argv)
 {
     if(argc > 1)
-        printf("%d\n", bracket_check(argv[1]));
+        printf("%d\n", math(argv[1]));
 }
--- math.c
+++ math.c
@@ -1,6 +1,8 @@
 // at some point add exponant and root functionality and then perhaps algebra
 // write brac location
 
+#include <stdlib.h>
+
 int		ft_atoi(const char *str);
 int     symbol_check(char *math_str);
 void    opperation_order(char **math_str);
@@ -11,6 +13,8 @@ int     brac_location(char *math_str);
 int     div_mult_location(char *math_str);
 int     add_sub_location(char *math_str);
 char    *ft_itoa(int n);
+void    opperate(int index, char **math_str);
+int     num_extract(char *math_str, int index);
 
 // a function to evaluate a mathematical string and return the interger answer
 int     math(char *math_str)
@@ -52,73 +56,87 @@ void    opperate(int index, char **math_str)
     int num1 = num_extract(*math_str, index);
     int num2;
     int i = index;
-    while(math_str[i])
+    while((*math_str)[i])
         i++;
     char *after_opp = (char *)malloc(i - index + 1);
     i = index;
-    while(math_str[i] && math_str[i] >= '0' && math_str[i] <= '9')
+    while((*math_str)[i] && (*math_str)[i] >= '0' && (*math_str)[i] <= '9')
         i++;
-    while(math_str[i] && math_str[i] >= '9' && math_str[i] <= '0')
+    while((*math_str)[i] && (*math_str)[i] >= '9' && (*math_str)[i] <= '0')
         i++;
     num2 = num_extract(*math_str, i);
-    while(math_str[index] != '*' || math_str[index] != '/' || math_str[index] != '+' || math_str[index] != '-' ||)
+    while((*math_str)[index] != '*' || (*math_str)[index] != '/' || (*math_str)[index] != '+' || (*math_str)[index] != '-')
         index++;
     // addition calculation
-    if(math_str[index] == '+')
+    if((*math_str)[index] == '+')
     {
-        while(math_str[i])
-            after_opp[p++] = math_str[i++];
+        while((*math_str)[i])
+            after_opp[p++] = (*math_str)[i++];
         re_assign = ft_itoa(num1 + num2);
         p = 0;
         while(re_assign[p])
-            math_str[index++] = re_assign[p++];
+            (*math_str)[index++] = re_assign[p++];
         p = 0;
         while(after_opp[p])
-            math_str[index++] = after_opp[p++];
-        math_str[index] = '\0';
+            (*math_str)[index++] = after_opp[p++];
+        (*math_str[index]) = '\0';
     }
     // minus calculation
-    else if(math_str[index] == '-')
+    else if((*math_str)[index] == '-')
     {
-        while(math_str[i])
-            after_opp[p++] = math_str[i++];
+        while((*math_str)[i])
+            after_opp[p++] = (*math_str)[i++];
         re_assign = ft_itoa(num1 - num2);
         p = 0;
         while(re_assign[p])
-            math_str[index++] = re_assign[p++];
+            (*math_str)[index++] = re_assign[p++];
         p = 0;
         while(after_opp[p])
-            math_str[index++] = after_opp[p++];
-        math_str[index] = '\0';
+            (*math_str)[index++] = after_opp[p++];
+        (*math_str)[index] = '\0';
     }
     //multiplication calculation
-    else if(math_str[index] == '*')
+    else if((*math_str)[index] == '*')
     {
-        while(math_str[i])
-            after_opp[p++] = math_str[i++];
+        while((*math_str)[i])
+            after_opp[p++] = (*math_str)[i++];
         re_assign = ft_itoa(num1 * num2);
         p = 0;
         while(re_assign[p])
-            math_str[index++] = re_assign[p++];
+            (*math_str)[index++] = re_assign[p++];
         p = 0;
         while(after_opp[p])
-            math_str[index++] = after_opp[p++];
-        math_str[index] = '\0';
+            (*math_str)[index++] = after_opp[p++];
+        (*math_str)[index] = '\0';
     }
     // division calculation
-    else if(math_str[index] == '/')
+    else if((*math_str)[index] == '/')
     {
-        while(math_str[i])
-            after_opp[p++] = math_str[i++];
+        while((*math_str)[i])
+            after_opp[p++] = (*math_str)[i++];
         re_assign = ft_itoa(num1 / num2);
         p = 0;
         while(re_assign[p])
-            math_str[index++] = re_assign[p++];
+            (*math_str)[index++] = re_assign[p++];
         p = 0;
         while(after_opp[p])
-            math_str[index++] = after_opp[p++];
-        math_str[index] = '\0';
+            (*math_str)[index++] = after_opp[p++];
+        (*math_str)[index] = '\0';
     }
+}
+
+int     num_extract(char *math_str, int index)
+{
+    char *str;
+    int i = index;
+    while(math_str[i + 1] && math_str[i] >= '0' && math_str[i] <= '9')
+        i++;
+    str = (char *)malloc(i + 1);
+    i = 0;
+    while(math_str[index + 1] && math_str[index] >= '0' && math_str[index] <= '9')
+        str[i++] = math_str[index];
+    str[i] = '\0';
+    return(ft_atoi(str));
 }
 
 // a function that returns the index of the first character in the mathematical opperation to process
@@ -162,7 +180,7 @@ int     div_mult_location(char *math_str)
 int     add_sub_location(char *math_str)
 {
     int i = 0;
-    while(math_str[i] != '+' || math_str != '-')
+    while(math_str[i] != '+' || math_str[i] != '-')
     {
         i++;
     }
@@ -247,15 +265,21 @@ int		ft_atoi(const char *str)
 	return (sign * res);
 }
 
-// interger to ascii converter
-char			*ft_itoa(int n)
-{
-	char	*ret;
-	int		q;
-	long	i;
-    int     temp;
 
-    temp = n;
+
+
+
+
+
+
+
+
+// interger to ascii converter
+static int		size(int n)
+{
+	long	i;
+	int		q;
+
 	q = 0;
 	i = 1;
 	if (n == 2147483647)
@@ -265,31 +289,49 @@ char			*ft_itoa(int n)
 		i = i * 10;
 		q++;
 	}
-    n = temp;
-	if (n == -2147483648)
-    {
-        int		i;
-	    int		c;
+	return (q);
+}
 
-        c = 100000000;
-        i = 0;
-        ret = (char *)malloc(12 * sizeof(char *));
-        if (ret == NULL)
-            return (NULL);
-        ret[i] = '-';
-        i++;
-        n = ((n + 8) / 10) * -1;
-        while (n > 0)
-        {
-            ret[i] = n / c + 48;
-            i++;
-            n = n - ((n / c) * c);
-            c = c / 10;
-        }
-        ret[i] = '8';
-        ret[i + 1] = '\0';
-        return (ret);
-    }
+static char		*neg(int n)
+{
+	int		i;
+	char	*ret;
+	int		c;
+
+	c = 100000000;
+	i = 0;
+	ret = (char *)malloc(12 * sizeof(char *));
+	if (ret == NULL)
+		return (NULL);
+	ret[i] = '-';
+	i++;
+	n = ((n + 8) / 10) * -1;
+	while (n > 0)
+	{
+		ret[i] = n / c + 48;
+		i++;
+		n = n - ((n / c) * c);
+		c = c / 10;
+	}
+	ret[i] = '8';
+	ret[i + 1] = '\0';
+	return (ret);
+}
+
+static char		*test(char *ret)
+{
+	ret[0] = '0';
+	ret[1] = '\0';
+	return (ret);
+}
+
+char			*ft_itoa(int n)
+{
+	char	*ret;
+	int		q;
+
+	q = size(n);
+	if (n == -2147483648)
 		return (neg(n));
 	ret = (char *)malloc(size(n) + 1 * sizeof(char *));
 	if (!ret)
@@ -302,11 +344,7 @@ char			*ft_itoa(int n)
 	}
 	ret[q] = '\0';
 	if (n == 0)
-    {
-    	ret[0] = '0';
-	    ret[1] = '\0';
-	    return (ret);
-    }
+		return (test(ret));
 	while (n != 0 && q-- >= 0)
 	{
 		ret[q] = n % 10 + '0';
